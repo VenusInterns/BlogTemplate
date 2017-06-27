a4e0e8c232b4c43ff33f709d57c7e0d656331d95
Jimmy Lewis <jamlew@microsoft.com>
2017-06-27 16:04:27 -0700

    Save a new post as XML

---
+++ BlogTemplate/Models/BlogDataStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace BlogTemplate.Models
+{
+    public class BlogDataStore
+    {
+        const string StorageFolder = "BlogFiles";
+        
+        public void SavePost(Post post)
+        {
+            Directory.CreateDirectory(StorageFolder);
+
+            string outputFilePath = $"{StorageFolder}\\{post.Slug}.xml";
+
+            XmlDocument doc = new XmlDocument();
+
+            XmlElement rootNode = doc.CreateElement("Post");
+            doc.AppendChild(rootNode);
+            rootNode.AppendChild(doc.CreateElement("Slug")).InnerText = post.Slug;
+            rootNode.AppendChild(doc.CreateElement("Title")).InnerText = post.Title;
+            rootNode.AppendChild(doc.CreateElement("Body")).InnerText = post.Body;
+
+            doc.Save(outputFilePath);
+        }
+    }
+}
