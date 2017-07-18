ef7b66262e2a9cb943659e8a766d79b82dae6fe9
Juliet Daniel <t-judani@microsoft.com>
2017-07-18 09:48:49 -0700

    Added new field, TagsString, to Post class

--- BlogTemplate/Models/Post.cs
+++ BlogTemplate/Models/Post.cs
@@ -17,6 +17,7 @@ namespace BlogTemplate.Models
         public string Body { get; set; }
         public DateTime PubDate { get; set; } = DateTime.Now;
         public DateTime LastModified { get; set; } = DateTime.Now;
+        public string TagsString { get; set; }
         public List<string> Tags { get; set; } = new List<string>();
         public string Slug{ get; set; }
         public bool IsPublic { get; set; }
