e5592d353944c39bafd2b9c2427064280dbd36f5
Juliet Daniel <t-judani@microsoft.com>
2017-07-19 16:45:44 -0700

    1. Generate forms for new posts and comments using tag helpers (instead of HTML helpers) 2. Require a min length for post title and body

--- BlogTemplate/Models/Post.cs
+++ BlogTemplate/Models/Post.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogTemplate.Models
 {
@@ -12,12 +13,16 @@ namespace BlogTemplate.Models
     {
         public List<Comment> Comments { get; set; } = new List<Comment>();
         [Required(ErrorMessage = "Title required")]
+        [MinLength(1)]
         public string Title { get; set; }
         [Required(ErrorMessage = "Post text required")]
+        [MinLength(1)]
         public string Body { get; set; }
         public DateTime PubDate { get; set; } = DateTime.Now;
         public DateTime LastModified { get; set; } = DateTime.Now;
         public string TagsString { get; set; }
+        // Add a custom model binder
+        //[ModelBinder(BinderType = typeof(TagsEntityBinder))]
         public List<string> Tags { get; set; } = new List<string>();
         public string Slug{ get; set; }
         public bool IsPublic { get; set; }
