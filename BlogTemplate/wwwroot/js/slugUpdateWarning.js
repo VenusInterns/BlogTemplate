7ad1ba490b9d63082efaaf6a9c5c697e8efed0f4
Juliet Daniel <t-judani@microsoft.com>
2017-08-01 14:40:32 -0700

    1. Separated JavaScript code for slug update warning into slugUpdateWarning.js. 2. Removed Reply link for comments until implementation.

---
+++ BlogTemplate/wwwroot/js/slugUpdateWarning.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var editpostform = document.querySelector("#editpost");
+
+    editpostform.addEventListener("submit", function (e) {
+        var titleElm = this.querySelector("#newPost_Title");
+        var oldtitle = titleElm.getAttribute("data-oldtitle");
+        var newtitle = titleElm.value;
+
+        if (oldtitle !== newtitle) {
+            if (confirm("Changing the post title will update the post slug and break external links. \r\rDo you wish to update the slug?")) {
+
+                this.querySelector("#updateslug").value = true;
+            }
+        }
+    }, false);
+
+})();   
