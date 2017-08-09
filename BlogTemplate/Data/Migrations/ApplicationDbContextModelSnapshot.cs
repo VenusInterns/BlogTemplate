00121ff4bd956515c90c86d722c3b62e49008a88
Juliet Daniel <t-judani@microsoft.com>
2017-08-09 14:14:33 -0700

    Added spaces at ends

--- BlogTemplate/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BlogTemplate/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -213,3 +213,4 @@ namespace BlogTemplate.Data.Migrations
         }
     }
 }
+
