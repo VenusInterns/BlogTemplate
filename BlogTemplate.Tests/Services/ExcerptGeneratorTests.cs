546a2e0fcb3e4734ae9454be4246c37254e0cf72
Juliet Daniel <t-judani@microsoft.com>
2017-08-09 15:10:59 -0700

    Wrote a test to check if given body text, when body text length does not exceed max length, then excerpt is the same as body.

--- BlogTemplate.Tests/Services/ExcerptGeneratorTests.cs
+++ BlogTemplate.Tests/Services/ExcerptGeneratorTests.cs
@@ -15,5 +15,13 @@ namespace BlogTemplate.Tests.Services
             string testExcerpt = testExcerptGenerator.CreateExcerpt("This is the body", 5);
             Assert.Equal("This ...", testExcerpt);
         }
+        [Fact]
+        public void CreateExcerpt_BodyLengthDoesNotExceedMaxLength_ExcerptEqualsBody()
+        {
+            BlogDataStore testDataStore = new BlogDataStore(new FakeFileSystem());
+            ExcerptGenerator testExcerptGenerator = new ExcerptGenerator();
+            string testExcerpt = testExcerptGenerator.CreateExcerpt("This is the body", 50);
+            Assert.Equal("This is the body", testExcerpt);
+        }
     }
 }
