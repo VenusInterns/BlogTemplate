538570aa5b5b520eec3d6c1050b428e4b3234ef2
Juliet Daniel <t-judani@microsoft.com>
2017-08-09 14:35:20 -0700

    Added spaces at ends

--- BlogTemplate/Program.cs
+++ BlogTemplate/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -23,3 +23,4 @@ namespace BlogTemplate
                 .Build();
     }
 }
+
