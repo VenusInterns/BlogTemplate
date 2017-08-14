dde1b0248f5ce21f84f376ede4d20b4ffbda5274
Juliet Daniel <julietcdaniel@gmail.com>
2017-08-14 16:34:17 -0700

    Configure services for SlugGenerator & ExcerptGenerator (#70)
    Register the SlugGenerator as an actual service in Startup.cs's ConfigureService method, and then consume it from the DI container wherever it's used.
    Added ExcerptGenerator service as a Singleton (rather than Scoped).
    Marked SlugGenerator and ExcerptGenerator as readonly as a best practice.
    Removed commented-out code.

--- BlogTemplate/Pages/Index.cshtml.cs
+++ BlogTemplate/Pages/Index.cshtml.cs
@@ -14,6 +14,7 @@ namespace BlogTemplate.Pages
         const string StorageFolder = "BlogFiles";
 
         private BlogDataStore _dataStore;
+
         public IEnumerable<PostSummaryModel> PostSummaries { get; private set; }
 
         public IndexModel(BlogDataStore dataStore)
