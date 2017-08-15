4c3c118b60299194fd908829073125ee1b45cca8
Juliet Daniel <t-judani@microsoft.com>
2017-08-15 15:35:48 -0700

    Started using regular expressions.

--- BlogTemplate/Services/SlugGenerator.cs
+++ BlogTemplate/Services/SlugGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BlogTemplate.Models;
 
@@ -20,6 +21,7 @@ namespace BlogTemplate.Services
         public string CreateSlug(string title)
         {
             string tempTitle = title;
+            Regex allowList = new Regex([^A-Za-z0-9-]);
             char[] invalidChars = Path.GetInvalidFileNameChars();
             foreach (char c in invalidChars)
             {
