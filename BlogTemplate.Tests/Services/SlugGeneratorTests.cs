77af9561ada2e0f93302f1a8b206f028423c4f5a
Juliet Daniel <t-judani@microsoft.com>
2017-08-17 15:23:34 -0700

    Used Theory and InlineData to clean up the test for removing invalid chars from slug.

--- BlogTemplate.Tests/Services/SlugGeneratorTests.cs
+++ BlogTemplate.Tests/Services/SlugGeneratorTests.cs
@@ -22,36 +22,24 @@ namespace BlogTemplate._1.Tests.Services
             Assert.Equal(test.Slug, "Test-title");
         }
 
-        [Fact]
-        public void CreateSlug_TitleContainsInvalidChars_RemoveInvalidCharsInSlug()
+        [Theory]
+        [InlineData("test?", "test")]
+        [InlineData("test<", "test")]
+        [InlineData("test>", "test")]
+        [InlineData("test/", "test")]
+        [InlineData("test&", "test")]
+        [InlineData("test!", "test")]
+        [InlineData("test#", "test")]
+        [InlineData("test''", "test")]
+        [InlineData("test|", "test")]
+        [InlineData("test©", "test")]
+        [InlineData("test%", "test")]
+        public void CreateSlug_TitleContainsInvalidChars_RemoveInvalidCharsInSlug(string input, string expected)
         {
             BlogDataStore testDataStore = new BlogDataStore(new FakeFileSystem());
             SlugGenerator testSlugGenerator = new SlugGenerator(testDataStore);
-            string slug1 = testSlugGenerator.CreateSlug("test?");
-            string slug2 = testSlugGenerator.CreateSlug("test<");
-            string slug3 = testSlugGenerator.CreateSlug("test>");
-            string slug4 = testSlugGenerator.CreateSlug("test/");
-            string slug5 = testSlugGenerator.CreateSlug("test&");
-            string slug6 = testSlugGenerator.CreateSlug("test!");
-            string slug7 = testSlugGenerator.CreateSlug("test#");
-            string slug8 = testSlugGenerator.CreateSlug("test''");
-            string slug9 = testSlugGenerator.CreateSlug("test|");
-            string slug10 = testSlugGenerator.CreateSlug("test©");
-            string slug11 = testSlugGenerator.CreateSlug("testα");
-            string slug12 = testSlugGenerator.CreateSlug("test%");
-            Assert.Equal("test", slug1);
-            Assert.Equal("test", slug2);
-            Assert.Equal("test", slug3);
-            Assert.Equal("test", slug4);
-            Assert.Equal("test", slug5);
-            Assert.Equal("test", slug6);
-            Assert.Equal("test", slug7);
-            Assert.Equal("test", slug8);
-            Assert.Equal("test", slug9);
-            Assert.Equal("test", slug10);
-            Assert.Equal("test", slug11);
-            Assert.Equal("test", slug12);
-        }
 
+            Assert.Equal(expected, testSlugGenerator.CreateSlug(input));
+        }
     }
 }
