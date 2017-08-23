37fd183f6fe135cad2a8b8d4f27831f038809a27
Juliet Daniel <t-judani@microsoft.com>
2017-08-23 14:25:36 -0700

    Changed Edit page to detect when a slug is changed, as applied to new ViewModels structure. Tried fiddling with JavaScript to detect when a post is being published to prompt with slug update warning only when appropriate.

--- BlogTemplate/wwwroot/js/slugUpdateWarning.js
+++ BlogTemplate/wwwroot/js/slugUpdateWarning.js
@@ -2,21 +2,22 @@
 
     var editpostform = document.querySelector("#editpost");
 
-    editpostform.addEventListener("submit", function (e) {
-        var titleElm = this.querySelector("#newPost_Title");
-        var oldtitle = titleElm.getAttribute("data-oldtitle");
-        var newtitle = titleElm.value;
+    editpostform.addEventListener("publish", function (e) {
 
-        if (oldtitle !== newtitle) {
-            if (confirm("Changing the post title will update the post slug and break external links. \r\rDo you wish to update the slug?")) {
+        editpostform.addEventListener("submit", function (e) {
+            var titleElm = this.querySelector("#EditedPost_Title");
+            var oldtitle = titleElm.getAttribute("data-oldtitle");
+            var newtitle = titleElm.value;
 
-                this.querySelector("#updateslug").value = true;
+            if (oldtitle !== newtitle) {
+                if (confirm("Changing the post title will update the post slug and break external links. \nDo you wish to update the slug?")) {
+                    this.querySelector("#updateSlug").value = true;
+                }
+                else {
+                    this.querySelector("#updateSlug").value = false;
+                }
             }
-            else
-            {
-                this.querySelector("#updateslug").value = false;
-            }
-        }
-    }, false);
 
+        }, false);
+    })
 })();   
