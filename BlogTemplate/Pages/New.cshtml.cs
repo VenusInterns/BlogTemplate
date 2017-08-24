5da27029fa4d5e8fbc7ecc2648480d25994134ab
Juliet Daniel <t-judani@microsoft.com>
2017-08-24 14:51:53 -0700

    Redirect to Drafts page when a new post is saved as a draft.

--- BlogTemplate/Pages/New.cshtml.cs
+++ BlogTemplate/Pages/New.cshtml.cs
@@ -52,7 +52,7 @@ namespace BlogTemplate._1.Pages
             if(ModelState.IsValid)
             {
                 SavePost(NewPost, false);
-                return Redirect("/Index");
+                return Redirect("/Drafts");
             }
 
             return Page();
