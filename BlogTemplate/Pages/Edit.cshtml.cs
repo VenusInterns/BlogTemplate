24b4e0ab44de443ba7350b498736d6c8e39152f1
t-luisaz <lisaza@stanford.edu>
2017-08-30 16:52:18 -0700

    Remove path to post

--- BlogTemplate/Pages/Edit.cshtml.cs
+++ BlogTemplate/Pages/Edit.cshtml.cs
@@ -24,7 +24,6 @@ namespace BlogTemplate._1.Pages
         }
 
         [BindProperty]
-        public string PathToPost { get; set; }
         public EditedPostModel EditedPost { get; set; }
 
         public void OnGet([FromRoute] int id)
@@ -43,8 +42,6 @@ namespace BlogTemplate._1.Pages
             {
                 RedirectToPage("/Index");
             }
-
-            PathToPost = $"/Post/{id}/{post.Slug}";
         }
 
         [ValidateAntiForgeryToken]
